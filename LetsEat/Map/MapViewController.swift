98b3e570e6ac88b68a3e97d63e666753208ee21d
JohnMHinckley <JMHinckley@comcast.net>
2024-04-09 11:46:08 -0400

    more work on map (Ch16)

--- LetsEat/Map/MapViewController.swift
+++ LetsEat/Map/MapViewController.swift
@@ -12,21 +12,43 @@ class MapViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet var mapView: MKMapView!
     
+    private let manager = MapDataManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        initialize()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func initialize() {
+        mapView.delegate = self
+        manager.fetch(completion: {(annotations) in setupMap(annotations)})
     }
-    */
+    
+    func setupMap (_ annotations: [RestaurantItem]) {
+        mapView.setRegion(manager.initialRegion(latDelta: 0.5, longDelta: 0.5), animated: true)
+        mapView.addAnnotations(manager.annotations)
+    }
 
+    func mapView (_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "custompin"
+        guard !annotation.isKind(of: MKUserLocation.self) else {
+            return nil
+        }
+        let annotationView: MKAnnotationView
+        if let customAnnotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) {
+            annotationView = customAnnotationView
+            annotationView.annotation = annotation } else {
+                let av = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                av.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+                annotationView = av
+        }
+        annotationView.canShowCallout = true
+        
+        if let image = UIImage(named: "custom-annotation") {
+            annotationView.image = image
+            annotationView.centerOffset = CGPoint(x: -image.size.width/2, y: -image.size.height/2)
+        }
+        return annotationView
+    }
 }
